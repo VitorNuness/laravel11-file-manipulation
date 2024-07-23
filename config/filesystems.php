ab91dd3a93982cb674b2cf67482b80b85a3f018e
Vitor Nunes da Silva <vitor1nuness@gmail.com>
2024-07-23 01:14:28 -0300

    add files disk storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,15 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
+        'files' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/files'),
+            'url' => env('APP_URL') . '/files',
             'visibility' => 'public',
             'throw' => false,
         ],
